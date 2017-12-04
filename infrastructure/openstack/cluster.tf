192f7fcc710f30c6c2a7c134b8c687764f3e1456
Tobias Hintze <tobias.hintze+git@travelping.com>
2017-12-04 21:29:34 +0100

    iface/mac pinning, os-keypair spec

--- infrastructure/openstack/cluster.tf
+++ infrastructure/openstack/cluster.tf
@@ -1,6 +1,7 @@
 # master node
 variable "name_prefix" {}
-variable "pvt_key" {}
+#variable "pvt_key" {}
+variable "instance_keypair" {}
 variable "count" {
   default = 2
 }
@@ -10,7 +11,7 @@ resource "openstack_compute_instance_v2" "test" {
   name            = "${format("%stest-%02d", var.name_prefix, count.index+1)}"
   image_name      = "ubuntu-16.04-image"
   flavor_name     = "x1.small"
-  key_pair        = "tli-nopw"
+ key_pair        = "${var.instance_keypair}"
   security_groups = ["all_traffic"]
 
   network {
@@ -28,19 +29,40 @@ resource "openstack_compute_instance_v2" "test" {
   connection {
       user = "ubuntu"
       type = "ssh"
-      private_key = "${file(var.pvt_key)}"
+      # private_key = "${file(var.pvt_key)}"
       timeout = "2m"
   }
 
-#  provisioner "remote-exec" {
-#      inline = [
-#        "export PATH=$PATH:/usr/bin",
-#        # install python for ansible support
+ provisioner "remote-exec" {
+      inline = [
+        "export PATH=$PATH:/usr/bin",
+        # install python for ansible support
 #        "sudo apt-get update",
 #        "sudo apt-get -y install python2.7",
-#        "sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1"
-#      ]
-#  }
+#        "sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1",
+#
+
+
+			# network setup
+		, <<EOF
+(
+printf 'auto lo\niface lo inet loopback\n\n'
+printf 'auto shared0\niface shared0 inet dhcp\n\n'
+printf 'auto uplink0\niface uplink0 inet dhcp\n\n'
+printf 'auto data0\n\n'
+) | sudo tee /etc/network/interfaces.d/50-cloud-init.cfg
+EOF
+
+			# udev rules for mac-pinned iface names
+		, <<EOF
+(
+printf 'SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="${self.network.0.mac}", NAME="shared0"\n'
+printf 'SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="${self.network.1.mac}", NAME="uplink0"\n'
+printf 'SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="${self.network.2.mac}", NAME="data0"\n'
+) | sudo tee /etc/udev/rules.d/71-persistent-net-ifaces.rules
+EOF
+      ]
+  }
 }
 
 resource "openstack_networking_network_v2" "cluster-network" {
@@ -53,4 +75,5 @@ resource "openstack_networking_subnet_v2" "cluster-network" {
   network_id = "${openstack_networking_network_v2.cluster-network.id}"
   cidr = "10.77.0.0/18"
   ip_version = 4
+	# FIXME/TODO - remove dhcp
 }
