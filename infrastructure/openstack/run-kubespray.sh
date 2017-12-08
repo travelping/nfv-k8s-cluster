fdc110d472952d172d4a037b501ebf3665fa362e
Tobias Hintze <tobias.hintze+git@travelping.com>
2017-12-08 12:51:05 +0100

    add multus-cni plugin

--- infrastructure/openstack/run-kubespray.sh
+++ infrastructure/openstack/run-kubespray.sh
@@ -9,3 +9,5 @@ git clone https://github.com/kubernetes-incubator/kubespray.git kubespray-$tag
 cd kubespray-$tag
 
 ansible-playbook -i ./inventory.cfg -b -v -e docker_dns_servers_strict=no cluster.yml
+
+ansible -i ./inventory.cfg kube-node -m shell -a "sudo docker run --rm -t -v /opt/cni/bin:/opt/cni/bin travelping/multus-cni install -m 0755 /src/bin/multus /opt/cni/bin/"
