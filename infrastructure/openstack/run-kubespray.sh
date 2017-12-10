d0af8faf9297e552439dc3133aea44fc5c4a0d10
Timo Lindhorst <timo.lindhorst@travelping.com>
2017-12-10 02:39:19 +0100

    Adjust `run-kubespray.sh`
    - use date in tag
    - provide tfstate as optional parameter
    - download admin.conf to artifacts

--- infrastructure/openstack/run-kubespray.sh
+++ infrastructure/openstack/run-kubespray.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -e
 
-tag=`date +%s`-$$
+tag=`date +%F`-$$
+
+tfstate=${1:-terraform.tfstate}
 
 git clone https://github.com/kubernetes-incubator/kubespray.git kubespray-$tag
 
-./inventory-from-tfstate.sh terraform.tfstate > kubespray-$tag/inventory.cfg
+./inventory-from-tfstate.sh $tfstate > kubespray-$tag/inventory.cfg
 
 cd kubespray-$tag
 
-ansible-playbook -i ./inventory.cfg -b -v -e docker_dns_servers_strict=no cluster.yml
+ansible-playbook -i ./inventory.cfg -b -v -e docker_dns_servers_strict=no -e kubeconfig_localhost=true cluster.yml
 
 ansible -i ./inventory.cfg kube-node -m shell -a "sudo docker run --rm -t -v /opt/cni/bin:/opt/cni/bin travelping/multus-cni install -m 0755 /src/bin/multus /opt/cni/bin/"
